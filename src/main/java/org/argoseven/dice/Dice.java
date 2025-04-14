65dc2f2ba172b0e8214b62a9c30ea0da76aa64d7
ArgoSeven <168479295+ArgoSeven@users.noreply.github.com>
2025-04-15 01:09:21 +0200

    removed unused import

--- src/main/java/org/argoseven/dice/Dice.java
+++ src/main/java/org/argoseven/dice/Dice.java
@@ -1,8 +1,11 @@
 package org.argoseven.dice;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+import net.minecraft.entity.player.PlayerEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 public class Dice implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("dice");
